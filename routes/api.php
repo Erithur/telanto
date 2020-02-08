e6dcef4a4ece12cbfb0124ba79d08bb2bbcd85a7
Eric <coboamoros@gmail.com>
2020-02-08 16:47:02 +0100

    Rutas de la api

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function () {
+
+    Route::get('/users/{user}/todos', 'UserController@todos');
+
+    Route::apiResource('todos', 'ToDoController')->only('store');
 });
