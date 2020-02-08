b7f1b815036a795c388aed2f425daccc4f17eb76
Eric <coboamoros@gmail.com>
2020-02-08 13:10:00 +0100

    MIgraciones
    Migraciones usuarios y todo

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,36 +1,51 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+    use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-    }
+    class CreateUsersTable extends Migration {
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('users');
+        /**
+         * Run the migrations.
+         *
+         * @return void
+        */
+
+        public function up() {
+
+            Schema::create('users', function (Blueprint $table) {
+
+                $table->increments('id');
+
+                $table->string('name');
+                $table->string('username');
+                $table->string('email')->unique();
+
+                $table->string('address_street');
+                $table->string('address_suite');
+                $table->string('address_city');
+                $table->string('address_zipcode');
+
+                $table->string('address_geo_lat');
+                $table->string('address_geo_lng');
+
+                $table->string('phone');
+                $table->string('website');
+
+                $table->string('company_name');
+                $table->string('company_catchPhrase');
+                $table->string('company_bs');
+            });
+        }
+
+        /**
+         * Reverse the migrations.
+         *
+         * @return void
+        */
+
+        public function down() {
+            Schema::dropIfExists('users');
+        }
     }
-}
--- database/migrations/2020_02_08_114032_create_to_dos_table.php
+++ database/migrations/2020_02_08_114032_create_to_dos_table.php
@@ -1,31 +1,41 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+    use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Support\Facades\Schema;
+    use Illuminate\Support\Facades\DB;
 
-class CreateToDosTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('to_dos', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->timestamps();
-        });
-    }
+    class CreateToDosTable extends Migration {
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('to_dos');
+        /**
+         * Run the migrations.
+         *
+         * @return void
+        */
+
+        public function up() {
+
+            Schema::create('to_do', function (Blueprint $table) {
+
+                $table->increments('id');
+
+                $table->unsignedInteger('userId');
+
+                $table->string('title');
+
+                $table->boolean('completed')->default('false');
+
+                $table->timestamp('createdOn')->default(DB::raw('CURRENT_TIMESTAMP'));
+            });
+        }
+
+        /**
+         * Reverse the migrations.
+         *
+         * @return void
+        */
+
+        public function down() {
+            Schema::dropIfExists('to_do');
+        }
     }
-}
